ee733247fcbf256e67ecb29508b524c98b9b0e25
Erik Perillo <erik.perillo@gmail.com>
2016-09-07 21:35:45 -0300

    structured again

---
+++ src/att.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python2.7
+
+import im
+import cvx
+import cv2
+import os
+import oarg
+
+def error(msg, code=1):
+    """
+    Prints error message and exits with code.
+    """
+    print "error:", msg
+    exit(code)
+
+def str_to_list(string, tp, delim=","):
+    """
+    Divides string separated by delimiter and converts to tp.
+    """
+    return map(tp, string.split(delim))
+
+def main():
+    #command-line arguments
+    img_file = oarg.Oarg("-i --img", "", "path to image", 0) 
+    pyr_lvl = oarg.Oarg("-p --pyr-lvl", 3, "levels for pyramid") 
+    #must be comma-separated
+    str_cs_ksizes= oarg.Oarg("-c --cs-ks", "3,7", 
+        "center-surround kernel sizes") 
+    #must be comma-separated
+    str_feats = oarg.Oarg("-f --features", "l1,l0,r,g,b,y", "features to use")
+    max_w = oarg.Oarg("-W --max-w", 800, "maximum width for image")
+    max_h = oarg.Oarg("-H --max-h", 600, "maximum hwight for image")
+    debug = oarg.Oarg("-d --debug", True, "debug mode")
+    save_dir = oarg.Oarg("-s --save-dir", ".", "directory to save images")
+    hlp = oarg.Oarg("-h --help", False, "this help message")
+
+    #parsing args
+    oarg.parse()
+
+    #help message
+    if hlp.val:
+        oarg.describeArgs("options:", def_val=True)
+        exit()
+
+    #checking validity of args
+    if not img_file.found:
+        error("image file not found (use -h for help)")
+
+    img = cv2.imread(img_file.val)
+
+    #checking validity of image
+    if img is None:
+        error("could not read image")
+    if len(img.shape) < 3:
+        error("image must be colored")
+
+    print "on file %s" % img_file.val
+    
+    #resizing image
+    img = cvx.resize(img, max_w.val, max_h.val)
+    #adapting image dimensions for proper pyramid up/downscaling
+    img = cvx.pyr_prepare(img, pyr_lvl.val)
+
+    #displaying original image
+    cvx.display(img, "original image", False)
+
+    #getting center-surround kernel sizes
+    cs_ksizes = str_to_list(str_cs_ksizes.val, int)
+    feats = str_to_list(str_feats.val, str)
+
+    #getting file name without extension
+    f_name = ".".join(os.path.basename(img_file.val).split(".")[:-1])
+
+    ims = []
+    #computing intensity maps
+    for feat in feats:
+        print "\ton feature '%s' ..." % feat
+        input_img = im.get_feature(img, feat)
+        db_im, imap = im.intensity_map(input_img, pyr_lvl.val, cs_ksizes,    
+            debug=debug)
+        
+        #displaying images
+        cvx.display(input_img, "input image %s" % feat)
+        cvx.display(imap, "intensity map %s" % feat)
+        if debug:
+            cvx.display(db_im, "intermediary intensity_map %s" % feat)
+
+        #appending partial intensity map
+        ims.append(imap)
+
+        #saving images
+        if save_dir.val:
+            cvx.save(imap, "%s/%s_%s_map.png" % (save_dir.val, f_name, feat))
+            if debug:
+                cvx.save(db_im, "%s/%s_%s_db_map.png" % \
+                    (save_dir.val, f_name, feat))
+
+    #computing final intensity map
+    final_im = sum(ims)
+    
+    #displaying final result
+    cvx.display(final_im, "final intensity map")
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    #saving final result
+    if save_dir.val:
+        cvx.save(final_im, "%s/%s_final_map.png" % (save_dir.val, f_name))
+
+if __name__ == "__main__":
+    main()
---
+++ src/im.py
@@ -0,0 +1,159 @@
+"""
+Module for intensity map calculations.
+"""
+
+import cv2
+import cvx
+import numpy as np
+
+#functions for getting components of Lab image. assumes image is already in Lab
+LAB_ATTR_FUNCS = {
+    "l1": lambda x: x[:, :, 0],
+    "l0": lambda x: cvx.inv(LAB_ATTR_FUNCS["l1"](x)),
+    "r": lambda x: x[:, :, 1],
+    "g": lambda x: cvx.inv(LAB_ATTR_FUNCS["r"](x)),
+    "y": lambda x: x[:, :, 2],
+    "b": lambda x: cvx.inv(LAB_ATTR_FUNCS["y"](x))
+}
+
+def get_feature(img, feat):
+    """
+    Gets feature map from image. Assumes image comes in BGR colorspace.
+    """
+    #preprocessing input
+    feat = feat.lower()
+
+    if feat in LAB_ATTR_FUNCS:
+        return get_lab_attr(img, attr=feat)
+
+    raise ValueError("unknown feature '%s'" % feat)
+
+def get_lab_attr(img, attr, cvt=True):
+    """
+    Gets Lab colorspace plane attr from image. Assumes img is in BGR.
+    """
+    #converting image to lab if required
+    if cvt:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
+
+    #checking validity of argument
+    if not attr in LAB_ATTR_FUNCS:
+        raise ValueError("invalid lab attr '%s' (use: %s)" % \
+            (attr, ",".join(LAB_ATTR_FUNCS)))
+
+    return LAB_ATTR_FUNCS[attr](img)
+
+def is_even(num):
+    """
+    Self-explanatory.
+    """
+    return num % 2 == 0
+
+def get_center_surround_kernel(size, dtype=np.float32):
+    """
+    Gets kernel to be used in convolution to perform center-surround
+    operation on image.
+    """
+    size = int(size)
+
+    #checking validity of arguments
+    if is_even(size):
+        raise InvalidDimensions("kernel size must be odd")
+    if size < 2:
+        raise InvalidDimensions("kernel size must be bigger than 1")
+    if dtype != np.float32 and dtype != np.float64:
+        raise InvalidDataType("kernel dtpe must be either float32 or float64")
+
+    #creating kernel
+    weight = -1.0/(size**2 - 1)
+    kernel = weight*np.ones(dtype=dtype, shape=(size, size))
+    kernel[size/2, size/2] = 1.0
+
+    return kernel
+
+def center_surround(img, kernel):
+    """
+    Performs center-surround operation on image with given kernel.
+    """
+    #checking validity of image dimensions
+    if len(img.shape) > 2:
+        raise InvalidDimensions("image must have depth one")
+
+    #applying operation through image
+    ddepth = cv2.CV_32F if kernel.dtype == np.float32 else cv2.CV_64F
+    cs_img = cv2.filter2D(img, ddepth, kernel)
+
+    return cs_img.clip(min=0.0)
+
+def im_pyr_one(x):
+    """
+    Default weight function of pyramid level for intensity_map.
+    """
+    return 1.0
+
+def im_cs_ksize_one(x):
+    """
+    Default weight function of center-surround kernel size for intensity_map.
+    """
+    return 1.0
+
+def im_weight_one(x, y):
+    """
+    Default weight function for intensity map.
+    """
+    return im_pyr_one(x)*im_cs_ksize_one(y)
+
+class IMLCWeightFunc(object):
+    """
+    Class for intensity map weight functions used for 
+    linear combinations of maps.
+    """
+    def __init__(self, pyr_f, cs_ksize_f):
+        self.pyr_f = pyr_f
+        self.cs_ksize_f = cs_ksize_f
+
+    def __call__(self, x, y):
+        return self.pyr_f(x)*self.cs_ksize_f(y)
+
+def intensity_map(img, pyr_lvl=3, cs_ksizes=(3, 7), weight_f=im_weight_one,
+    dtype=np.float32, debug=False):
+    """
+    Gets intensity map by by summing up center_surround on
+    multiple scales, and kernel sizes.
+    If debug is True, returns image with intermediate results.
+    """
+    #getting all kernels
+    cs_kernels = [get_center_surround_kernel(ks, dtype) for ks in cs_ksizes]
+    #initial value for intensity map
+    im_img = np.zeros(dtype=dtype, shape=img.shape)
+    #debug image
+    debug_img = None
+
+    #iterating over pyramid levels (from 0 to pyr_lvl)
+    for i in xrange(pyr_lvl+1):
+        #getting downsampled image
+        img = cv2.pyrDown(img) if i > 0 else img
+        #partial debug image
+        db_img = None
+
+        for ki, k in enumerate(cs_kernels):
+            #getting center-surround image 
+            cs_img = center_surround(img, weight_f(pyr_lvl, ki)*k)
+            #updating debug partial image
+            if debug:
+                db_img = cs_img if db_img is None else \
+                    cvx.h_append(db_img, cs_img)
+            #rescaling into original image dimensions
+            cs_img = cvx.rep_pyrUp(cs_img, i)
+            #updating debug partial image
+            if debug and i > 0:
+                db_img = cvx.h_append(db_img, cs_img)
+            #summing contribution
+            im_img += cs_img
+    
+        #updating debug image
+        if debug:
+            debug_img = db_img if debug_img is None else \
+                cvx.v_append(debug_img, db_img) 
+
+    return debug_img, im_img
