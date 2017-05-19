9405c8c1d4d123246f6ad0f27603744b01520918
Erik Perillo <erik.perillo@gmail.com>
2017-05-18 21:56:57 -0300

    update on template model in config dir

--- att/deep/config.template.d/model.py
+++ att/deep/config.template.d/model.py
@@ -1,11 +1,48 @@
 import lasagne
 import numpy as np
 from theano import tensor as T
+from theano.compile.nanguardmode import NanGuardMode
+
+def set_layer_as_immutable(layer):
+    """Sets layer parameters so as not to be modified in training steps."""
+    for k in layer.params.keys():
+        layer.params[k] -= {"regularizable", "trainable"}
+
+def std_norm(x):
+    """Mean-std normalization."""
+    return (x - T.mean(x))/T.std(x)
+
+def cov(a, b):
+    return T.mean((a - T.mean(a))*(b - T.mean(b)))
+
+def corr_coef(pred, tgt):
+    """Correlation Coefficient."""
+    return cov(pred, tgt)/(T.std(pred)*T.std(tgt))
+
+def r_squared(pred, tgt):
+    """R-squared."""
+    return T.square(coef_corr(pred, tgt))
+
+def similarity(pred, tgt):
+    """Similarity."""
+    return T.sum(T.minimum(pred/pred.sum(), tgt/tgt.sum()))
+
+def mse(pred, tgt):
+    """Mean-squared-error."""
+    return lasagne.objectives.squared_error(pred, tgt).mean()
+
+def norm_mse(pred, tgt, alpha):
+    """Normalized mean-squared-error."""
+    return T.square((pred/pred.max() - tgt)/(alpha - tgt)).mean()
+
+def mae(pred, tgt):
+    """Mean-absolute-error."""
+    return T.mean(abs(pred - tgt))
 
 class Model:
     #in format depth, rows, cols
-    INPUT_SHAPE = (3, 192, 256)
-    OUTPUT_SHAPE = (1, 12, 16)
+    INPUT_SHAPE = (3, 120, 160)
+    OUTPUT_SHAPE = (1, 30, 40)
 
     def __init__(self, input_var=None, target_var=None, load_net_from=None):
         self.input_var = T.tensor4('inps') if input_var is None else input_var
@@ -23,15 +60,14 @@ class Model:
             deterministic=True)
 
         #loss train/test symb. functionS
-        self.train_loss = lasagne.objectives.squared_error(self.train_pred,
-            self.target_var).mean()
+        self.train_loss = -corr_coef(self.train_pred, self.target_var)
+        #self.train_loss = mse(self.train_pred, self.target_var)
         #optional regularization term
         #reg = lasagne.regularization.regularize_network_params(
         #    self.net["output"],
         #    lasagne.regularization.l2)
         #self.train_loss += reg*0.00001
-        self.test_loss = lasagne.objectives.squared_error(self.test_pred,
-            self.target_var).mean()
+        self.test_loss = -corr_coef(self.test_pred, self.target_var)
 
         #updates symb. function for gradient descent
         self.params = lasagne.layers.get_all_params(self.net["output"],
@@ -40,7 +76,7 @@ class Model:
             self.train_loss, self.params, learning_rate=0.01, momentum=0.9)
 
         #mean absolute error
-        self.mae = T.mean(abs(self.test_pred - self.target_var))
+        self.mae = mae(self.test_pred, self.target_var)
 
     def get_net_model(self, input_var=None, inp_shp=None):
         """
@@ -57,38 +93,53 @@ class Model:
 
         #convpool layer
         net["conv1"] = lasagne.layers.Conv2DLayer(net["input"],
+            num_filters=10, filter_size=(3, 3),
+            nonlinearity=lasagne.nonlinearities.tanh,
+            pad="same")
+        net["conv2"] = lasagne.layers.Conv2DLayer(net["conv1"],
+            num_filters=10, filter_size=(3, 3),
+            nonlinearity=lasagne.nonlinearities.tanh,
+            pad="same")
+        net["conv3"] = lasagne.layers.Conv2DLayer(net["conv2"],
+            num_filters=12, filter_size=(3, 3),
+            nonlinearity=lasagne.nonlinearities.tanh,
+            pad="same")
+        net["pool1"] = lasagne.layers.MaxPool2DLayer(net["conv3"],
+            pool_size=(2, 2))
+
+        #convpool layer
+        net["conv4"] = lasagne.layers.Conv2DLayer(net["pool1"],
+            num_filters=10, filter_size=(3, 3),
+            nonlinearity=lasagne.nonlinearities.tanh,
+            pad="same")
+        net["conv5"] = lasagne.layers.Conv2DLayer(net["conv4"],
+            num_filters=10, filter_size=(3, 3),
+            nonlinearity=lasagne.nonlinearities.tanh,
+            pad="same")
+        net["conv6"] = lasagne.layers.Conv2DLayer(net["conv5"],
+            num_filters=12, filter_size=(3, 3),
+            nonlinearity=lasagne.nonlinearities.tanh,
+            pad="same")
+        net["pool2"] = lasagne.layers.MaxPool2DLayer(net["conv6"],
+            pool_size=(2, 2))
+
+        #conv layer
+        net["conv7"] = lasagne.layers.Conv2DLayer(net["pool2"],
+            num_filters=16, filter_size=(3, 3),
+            nonlinearity=lasagne.nonlinearities.tanh,
+            pad="same")
+        #conv layer
+        net["conv8"] = lasagne.layers.Conv2DLayer(net["conv7"],
             num_filters=32, filter_size=(3, 3),
-            nonlinearity=lasagne.nonlinearities.rectify,
+            nonlinearity=lasagne.nonlinearities.tanh,
             pad="same")
-        net["pool1"] = lasagne.layers.MaxPool2DLayer(net["conv1"],
-            pool_size=(2, 2))
-
-        #convpool layer
-        net["conv2"] = lasagne.layers.Conv2DLayer(net["pool1"],
-            num_filters=48, filter_size=(3, 3),
-            nonlinearity=lasagne.nonlinearities.rectify,
+        net["conv9"] = lasagne.layers.Conv2DLayer(net["conv8"],
+            num_filters=64, filter_size=(5, 5),
+            nonlinearity=lasagne.nonlinearities.tanh,
             pad="same")
-        net["pool2"] = lasagne.layers.MaxPool2DLayer(net["conv2"],
-            pool_size=(2, 2))
-
-        #convpool layer
-        net["conv3"] = lasagne.layers.Conv2DLayer(net["pool2"],
-            num_filters=64, filter_size=(3, 3),
-            nonlinearity=lasagne.nonlinearities.rectify,
-            pad="same")
-        net["pool3"] = lasagne.layers.MaxPool2DLayer(net["conv3"],
-            pool_size=(2, 2))
-
-        #convpool layer
-        net["conv4"] = lasagne.layers.Conv2DLayer(net["pool3"],
-            num_filters=80, filter_size=(3, 3),
-            nonlinearity=lasagne.nonlinearities.rectify,
-            pad="same")
-        net["pool4"] = lasagne.layers.MaxPool2DLayer(net["conv4"],
-            pool_size=(2, 2))
 
         #output
-        net["output"] = lasagne.layers.Conv2DLayer(net["pool4"],
+        net["output"] = lasagne.layers.Conv2DLayer(net["conv9"],
             num_filters=1, filter_size=(1, 1),
             nonlinearity=lasagne.nonlinearities.identity)
 
