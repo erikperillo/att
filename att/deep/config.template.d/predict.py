9a490eefd00c005e6a71836329580a7b5471c913
Erik Perillo <erik.perillo@gmail.com>
2017-05-19 13:01:57 -0300

    config update

--- att/deep/config.template.d/predict.py
+++ att/deep/config.template.d/predict.py
@@ -3,28 +3,31 @@ Configuration file for predict script.
 """
 
 import os
+import glob
 
 from . import train
 from . import datapreproc
 
-#directory where dataset is stored. can be empty
-dataset_dirpath = os.path.dirname(train.dataset_filepath)
+_data_dir_path = "/home/erik/proj/att/att/deep/data"
 #filepath of dataset statistics (mean/std etc)
-dataset_stats_filepath = os.path.join(dataset_dirpath, "data_stats.gz")
+dataset_stats_filepath = os.path.join(_data_dir_path,
+    "judd_cat2000_dataset/data_stats.pkl")
 #filepath of model
-model_filepath = os.path.join(train.output_dir_basedir, "train", "model.npz")
+model_filepath = os.path.join(_data_dir_path, "train_11", "model.npz")
 
-#normalize images per channel
-normalize_per_channel = datapreproc.x_normalize_per_channel
-#normalization method (std, unit, nonde)
-norm_method = datapreproc.x_normalization
 #colorspace to transform image
 img_colspace = datapreproc.x_img_colspace
-#if true, crops image before resizing so as to get the same aspect ratio
-#as the target shape
+#normalization method (std, unit, nonde)
+img_normalization = datapreproc.x_normalization
+#normalize per image
+normalize_per_image = datapreproc.x_normalize_per_image
+#if true, crops image before resizing for the same aspect ratio as target shape
 crop_on_resize = True
 #if true, swaps axis
 swap_img_channel_axis = datapreproc.swap_channel_axis
+
+#data type
+img_dtype = datapreproc.x_dtype
 
 #if true, saves each prediction as <filepath>_fixmap.png
 save_preds = False
--- att/deep/config.template.d/train.py
+++ att/deep/config.template.d/train.py
@@ -1,24 +1,26 @@
 import os
 import glob
+import numpy as np
 
 from . import datapreproc
 
+_data_dir_path = "/home/erik/proj/att/att/deep/data"
 #directory where dir with train info/model will be stored
-output_dir_basedir = os.path.join(datapreproc.output_dir_basedir,
-    datapreproc.dataset_name + "_dataset_2")
+output_dir_basedir = _data_dir_path
 
-_dataset_filepaths = glob.glob(os.path.join(output_dir_basedir,"data_part*.gz"))
+_dataset_filepaths = glob.glob(os.path.join(_data_dir_path,
+    "salicon_judd_cat2000_dataset_1", "data_part*.gz"))
 #filepaths of train batches
-dataset_train_filepaths = _dataset_filepaths[0:2]
+dataset_train_filepaths = _dataset_filepaths[0:-2]
 #filepaths of validation batches
-dataset_val_filepaths = None#_dataset_filepaths[2:3]
+dataset_val_filepaths = _dataset_filepaths[-2:]
 #if not None, ignores dataset_{train,val}_filepaths and uses this as source
 dataset_filepath = None#_dataset_filepaths[-1]
 #validation fraction. ignored if dataset_filepath is not used
-val_frac = 0.1
+val_frac = None#0.1
 
 #number of epochs to use in train
-n_epochs = 2
+n_epochs = 1
 #batch size
 batch_size = 10
 #maximum number of iterations
@@ -27,3 +29,6 @@ max_its = None
 verbose = 2
 #validation function value tolerance
 val_f_val_tol = None
+#data types
+x_dtype = np.float32
+y_dtype = np.float32
